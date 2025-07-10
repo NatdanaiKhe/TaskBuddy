72565bd25dd82755b8cd83f7a80d8e20734230cc
NatdanaiKhe <natdanai.khemt@gmail.com>
2025-07-10 13:22:00 +0700

    [fix] back: fix refresh accessToken with refreshToken

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -9,7 +9,7 @@ const authController = new AuthController();
 router.post("/login", authController.login);
 router.get("/me", authenticate, userController.getUserById);
 router.post("/register", authController.register);
-router.post("/refresh-token", authenticate, authController.refresh);
+router.post("/refresh-token", authController.refresh);
 router.post("/verify-email/:token", authController.verifyEmail);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
