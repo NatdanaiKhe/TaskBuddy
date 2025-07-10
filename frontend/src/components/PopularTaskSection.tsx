1b38b895f704a0d7c8ad53188edcd170a4db2813
NatdanaiKhe <natdanai.khemt@gmail.com>
2025-07-10 14:37:56 +0700

    [update] front:  fetching a task on popular section

--- frontend/src/components/PopularTaskSection.tsx
+++ frontend/src/components/PopularTaskSection.tsx
@@ -1,23 +1,51 @@
-
+import { useEffect, useState } from "react";
 import TaskCard from "./TaskCard";
-import { tasks } from "@/mock";
+import taskService from "@/api/taskService";
+import type { Task } from "@/types/taskTypes";
+import Loader from "./Loader";
 function PopularTaskSection() {
-  
+  const [loading, setLoading] = useState(false);
+  const [tasks, setTasks] = useState<Task[]>();
+
+  const fetchLatestTasks = async () => {
+    try {
+      setLoading(true);
+      const res = await taskService.getAllTask({ limit: 4 });
+      if (!res.ok) {
+        console.log("error");
+      }
+
+      setTasks(res.data.tasks);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchLatestTasks();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if(!tasks){
+    return <h1>not found</h1>
+  }
   return (
-    <section className="py-16 bg-gray-50">
+    <section className="bg-gray-50 py-16">
       <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center mb-12">
+        <div className="mb-12 flex items-center justify-between">
           <h2 className="text-3xl font-bold text-gray-800">Popular Tasks</h2>
-          <a href="#" className="text-blue-600 font-medium hover:text-blue-700">
+          <a href="/tasks" className="font-medium text-blue-600 hover:text-blue-700">
             View All
           </a>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           {tasks.map((task, index) => (
-            <TaskCard
-              key={index}
-              {...task}
-            />
+            <TaskCard key={index} {...task} />
           ))}
         </div>
       </div>
