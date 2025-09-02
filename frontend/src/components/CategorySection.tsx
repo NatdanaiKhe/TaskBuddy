ccc5c8f0c1f53c389c898712dad7164ac4304b4c
Natdanai Khemtong <natdanai.khemt@gmail.com>
2025-09-02 20:12:18 +0700

    [fix] (front) : fix overflow items

--- frontend/src/components/CategorySection.tsx
+++ frontend/src/components/CategorySection.tsx
@@ -8,14 +8,14 @@ function CategorySection() {
         <h2 className="mb-12 text-center text-3xl font-bold text-gray-800">
           Browse by Category
         </h2>
-        <div className="flex items-center justify-center gap-8">
+        <div className="flex flex-wrap items-center justify-center gap-8">
           {categories.map((category, index) => (
-            <a key={index} href={`/tasks?category=${category.value}`}>
-              <div className="flex cursor-pointer flex-col items-center">
+            <a key={index} href={`/tasks?category=${category.value}`} className="flex-1 basis-40 max-w-[100px]">
+              <div className="flex flex-1  cursor-pointer flex-col items-center">
                 <div className={`${category.color} mb-4 rounded-full p-4`}>
                   {category.icon}
                 </div>
-                <h3 className="text-lg font-medium text-gray-700">
+                <h3 className="text-lg text-center font-medium text-gray-700">
                   {category.name}
                 </h3>
               </div>
